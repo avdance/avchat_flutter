09367586550431a1711af7a6b469ce2b82e34cf6
wangzhumo <wangzhumoo@gmail.com>
2019-12-03 21:06:11 +0800

    [feature]add ios project

---
+++ avchat_flutter_ios/IOSProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSProject
+//
+//  Created by 王诛魔 on 2019/12/3.
+//  Copyright © 2019 王诛魔. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
