6f4e0b2ca679e0c67d3bdb1836519871bb9a249d
wangzhumo <wangzhumoo@gmail.com>
2019-12-06 23:09:26 +0800

    [feature]用fish_redux重构router

--- lib/routers/application.dart
+++ lib/routers/application.dart
@@ -1,5 +1,13 @@
-import 'package:fluro/fluro.dart';
+import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart' hide Action;
 
 class Application{
-  static Router router;
+  static AbstractRoutes router;
+
+
+  static MaterialPageRoute<Object> generateRoute(RouteSettings settings){
+    return MaterialPageRoute<Object>(builder: (BuildContext context) {
+      return router.buildPage(settings.name, settings.arguments);
+    });
+  }
 }
--- lib/routers/routes.dart
+++ lib/routers/routes.dart
@@ -1,20 +1,88 @@
-import 'package:fluro/fluro.dart';
-import 'package:flutter/material.dart';
-import 'package:avchat_flutter/routers/router_handlers.dart';
+import 'package:avchat_flutter/store/state.dart';
+import 'package:avchat_flutter/store/store.dart';
+import 'package:avchat_flutter/views/login/page.dart';
+import 'package:avchat_flutter/views/splash/page.dart';
+import 'package:fish_redux/fish_redux.dart';
 
-class Routers{
-
-  static String root = "/";
+class Routers {
+  static String splash = "/";
+  static String login = "/login";
   static String home = "/home";
 
+  /// 创建应用的根 Widget
+  /// 1. 创建一个简单的路由，并注册页面
+  /// 2. 对所需的页面进行和 AppStore 的连接
+  /// 3. 对所需的页面进行 AOP 的增强
+  static AbstractRoutes configureRoutes() {
+    final AbstractRoutes routes = PageRoutes(
+      pages: <String, Page<Object, dynamic>>{
+        //注册各个页面
+        splash : SplashPage(),
+        login: LoginPage()
+      },
+      visitor: (String path, Page<Object, dynamic> page) {
+        /// 只有特定的范围的 Page 才需要建立和 AppStore 的连接关系
+        /// 满足 Page<T> ，T 是 GlobalBaseState 的子类
+        if (page.isTypeof<GlobalBaseState>()) {
+          /// 建立 AppStore 驱动 PageStore 的单向数据连接
+          /// 1. 参数1 AppStore
+          /// 2. 参数2 当 AppStore.state 变化时, PageStore.state 该如何变化
+          page.connectExtraStore<GlobalState>(GlobalStore.store,
+              (Object pagestate, GlobalState appState) {
+            final GlobalBaseState p = pagestate;
+            if (p.themeColor != appState.themeColor) {
+              if (pagestate is Cloneable) {
+                final Object copy = pagestate.clone();
+                final GlobalBaseState newState = copy;
+                newState.themeColor = appState.themeColor;
+                return newState;
+              }
+            }
+            return pagestate;
+          });
+        }
 
+        /// AOP
+        /// 页面可以有一些私有的 AOP 的增强， 但往往会有一些 AOP 是整个应用下，所有页面都会有的。
+        /// 这些公共的通用 AOP ，通过遍历路由页面的形式统一加入。
+        page.enhancer.append(
+          /// View AOP
+          viewMiddleware: <ViewMiddleware<dynamic>>[
+            safetyView<dynamic>(),
+          ],
 
-  static void configureRoutes(Router router){
-    router.notFoundHandler = new Handler(
-      handlerFunc: (BuildContext context,Map<String,List<String>> params){
-        print("ROUTER NOT FIND!");
-      });
+          /// Adapter AOP
+          adapterMiddleware: <AdapterMiddleware<dynamic>>[
+            safetyAdapter<dynamic>()
+          ],
 
-    router.define(root,handler:splashHandler);
+          /// Effect AOP
+          effectMiddleware: <EffectMiddleware<dynamic>>[
+            _pageAnalyticsMiddleware<dynamic>(),
+          ],
+
+          /// Store AOP
+          middleware: <Middleware<dynamic>>[
+            logMiddleware<dynamic>(tag: page.runtimeType.toString()),
+          ],
+        );
+      },
+    );
+    return routes;
   }
+}
+
+/// 简单的 Effect AOP
+/// 只针对页面的生命周期进行打印
+EffectMiddleware<T> _pageAnalyticsMiddleware<T>({String tag = 'redux'}) {
+  return (AbstractLogic<dynamic> logic, Store<T> store) {
+    return (Effect<dynamic> effect) {
+      return (Action action, Context<dynamic> ctx) {
+        if (logic is Page<dynamic, dynamic> && action.type is Lifecycle) {
+          print('${logic.runtimeType} ${action.type.toString()} ');
+        }
+        return effect?.call(action, ctx);
+      };
+    };
+  };
 }
