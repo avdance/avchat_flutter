4b32015d055d99abdb2d8d9720915a75b896f3b1
wangzhumo <wangzhumoo@gmail.com>
2019-12-14 19:43:55 +0800

    [feature]主页 profile   -7

---
+++ lib/views/profile/state.dart
@@ -0,0 +1,13 @@
+import 'package:fish_redux/fish_redux.dart';
+
+class ProfileState implements Cloneable<ProfileState> {
+
+  @override
+  ProfileState clone() {
+    return ProfileState();
+  }
+}
+
+ProfileState initState(Map<String, dynamic> args) {
+  return ProfileState();
+}
