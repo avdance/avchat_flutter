376511584b9c13945d078260d5cc6c9a71eadbc4
wangzhumo <wangzhumoo@gmail.com>
2019-12-14 13:22:16 +0800

    [feature]自动生成

---
+++ lib/api/base_response.dart
@@ -0,0 +1,46 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'dart:convert';
+
+/// 基础返回
+class BaseResponse {
+  String msg;
+  String token;
+  int errCode;
+  int code;
+  Map<String, dynamic> data;
+
+  BaseResponse(int code, {msg});
+
+  BaseResponse.fromJson(String value) {
+    Map<String, dynamic> jsonObject = json.decode(value);
+    msg = jsonObject['msg'];
+    code = jsonObject['code'];
+    data = jsonObject['data'];
+    token = jsonObject['token'];
+    errCode = jsonObject['errCode'];
+  }
+
+  bool isSuccessful() {
+    println("msg = $msg  ,code = $code   errCode = $errCode");
+    if (code == 0 && errCode <= 0) {
+      return true;
+    } else if (code == 0 && errCode > 0) {
+      return false;
+    } else if (code < 0) {
+      return false;
+    }
+    return false;
+  }
+
+  Map<String, dynamic> getData() {
+    return data;
+  }
+
+  String getToken() {
+    return token;
+  }
+
+  String getMessage() {
+    return msg;
+  }
+}
