cff2484b8980cd739344527fff425ca07c9835e9
wangzhumo <wangzhumoo@gmail.com>
2019-12-21 22:54:09 +0800

    [feature]index_bar view

--- lib/widgets/index_bar.dart
+++ lib/widgets/index_bar.dart
@@ -33,43 +33,48 @@ const List<String> INDEX_OF_LETTERS = const [
 ];
 
 ///IndexItem
-class IndexItem{
+class IndexItem {
   String text;
   int position;
   bool isSelected;
 
-  IndexItem({this.text,this.position,this.isSelected});
+  IndexItem({this.text, this.position, this.isSelected});
 }
 
-
 /// IndexView
-class IndexBarLayout extends StatefulWidget{
-
+class IndexBarLayout extends StatefulWidget {
   /// index data.
   final List<String> dataSet;
+
   /// Index width
   final double itemWidth;
+
   /// Index item height
   final double itemHeight;
+
   /// Background color
   final Color color;
+
   /// IndexBar text style.
   final TextStyle textStyle;
   final TextStyle textStyleSelected;
 
   ///回调函数
-  final List<OnItemSelectedListener> _listeners = new List<OnItemSelectedListener>();
+  final List<OnItemSelectedListener> _listeners =
+      new List<OnItemSelectedListener>();
   final OnItemSelectedListener onSelected;
 
   ///需要的参数
-  IndexBarLayout({Key key,
-      this.dataSet = INDEX_OF_LETTERS,
-      this.onSelected,
-    this.itemWidth,
-    this.itemHeight,
+  IndexBarLayout({
+    Key key,
+    this.dataSet = INDEX_OF_LETTERS,
+    this.onSelected,
+    this.itemWidth = 16.0,
+    this.itemHeight = 16.0,
     this.color = Colors.transparent,
-    this.textStyle = const TextStyle(fontSize: 11.0,color: Colors.black45),
-    this.textStyleSelected = const TextStyle(fontSize: 11.0,color: Colors.black),
+    this.textStyle = const TextStyle(fontSize: 11.0, color: Colors.black45),
+    this.textStyleSelected =
+        const TextStyle(fontSize: 11.0, color: Colors.black),
   });
 
   @override
@@ -79,51 +84,58 @@ class IndexBarLayout extends StatefulWidget{
 }
 
 ///整个Layout.
-class _IndexItemLayoutState extends State<IndexBarLayout>{
-
+class _IndexItemLayoutState extends State<IndexBarLayout> {
   List<Widget> _children = new List<Widget>();
   int _selectedIndex;
 
-  int _getIndex(int offset){
+  int _getIndex(int offset) {
     //根据位移获取到现在的Index.
-
-  }
-
-  @override
-  void initState() {
-    //state init
-    widget.dataSet.forEach((index){
-      //创建这些View出来.
-
-    });
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: _ItemView(
+    double layoutHeight = (widget.dataSet.length * widget.itemHeight);
+    for (int index = 0; index < widget.dataSet.length; index++) {
+      String strKey = widget.dataSet[index];
+      _children.add(ItemView(
+        itemData: IndexItem(text: strKey, position: index, isSelected: false),
+        itemHeight: widget.itemHeight,
+        itemWidth: widget.itemWidth,
+        textStyle: widget.textStyle,
+        textStyleSelected: widget.textStyleSelected,
+        color: widget.color,
+        colorSelected: Colors.blueAccent,
+        onSelected: widget.onSelected,
+      ));
+    }
 
+    return Container(
+      width: widget.itemWidth,
+      height: layoutHeight,
+      alignment: Alignment.center,
+      child: Column(
+        children: _children,
       ),
     );
   }
-
 }
 
 ///实际上每一条ItemView.
-class _ItemView extends StatefulWidget{
-
+class ItemView extends StatefulWidget {
   ///需要知道的字段有
   /// Index width
   final double itemWidth;
+
   /// Index item height
   final double itemHeight;
+
   /// IndexBar text style.
   final TextStyle textStyle;
   final TextStyle textStyleSelected;
+
   ///自己的信息
   final IndexItem itemData;
+
   ///背景色
   final Color color;
   final Color colorSelected;
@@ -131,24 +143,26 @@ class _ItemView extends StatefulWidget{
   final OnItemSelectedListener onSelected;
 
   ///需要的参数
-  _ItemView({Key key,
+  ItemView({
+    Key key,
     @required this.itemData,
     this.onSelected,
     this.itemWidth = 16.0,
     this.itemHeight = 16.0,
     this.color = Colors.transparent,
     this.colorSelected = Colors.blue,
-    this.textStyle = const TextStyle(fontSize: 11.0,color: Colors.black45),
-    this.textStyleSelected = const TextStyle(fontSize: 11.0,color: Colors.black),
+    this.textStyle = const TextStyle(fontSize: 11.0, color: Colors.black45),
+    this.textStyleSelected =
+        const TextStyle(fontSize: 11.0, color: Colors.black),
   });
 
   @override
   State<StatefulWidget> createState() {
-    return _ItemViewState();
+    return ItemViewState();
   }
 }
 
-class _ItemViewState extends State<_ItemView>{
+class ItemViewState extends State<ItemView> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -157,20 +171,21 @@ class _ItemViewState extends State<_ItemView>{
         width: widget.itemWidth,
         height: widget.itemHeight,
         decoration: BoxDecoration(
-            color: widget.itemData.isSelected?widget.colorSelected:widget.color,
-            borderRadius: BorderRadius.circular(widget.itemWidth/2)
-        ),
-        child: Text(widget.itemData.text,
+            color: widget.itemData.isSelected
+                ? widget.colorSelected
+                : widget.color,
+            borderRadius: BorderRadius.circular(widget.itemWidth)),
+        child: Text(
+          widget.itemData.text,
           textAlign: TextAlign.center,
-          style: widget.itemData.isSelected?widget.textStyleSelected:widget.textStyle,
+          style: widget.itemData.isSelected
+              ? widget.textStyleSelected
+              : widget.textStyle,
         ),
       ),
     );
   }
 }
 
-
 ///当Item选中后,回调给外部.
 typedef void OnItemSelectedListener(IndexItem item);
-
-
