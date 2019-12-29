4c2681e7d301096365e06e3d86236c64372a810c
wangzhumo <wangzhumoo@gmail.com>
2019-12-29 20:49:03 +0800

    [feature]完成添加好友页面 -1

--- lib/views/addfriend/view.dart
+++ lib/views/addfriend/view.dart
@@ -4,6 +4,30 @@ import 'package:flutter/material.dart';
 import 'action.dart';
 import 'state.dart';
 
-Widget buildView(AddFriendState state, Dispatch dispatch, ViewService viewService) {
-  return Container();
+Widget buildView(
+    AddFriendState state, Dispatch dispatch, ViewService viewService) {
+  return Scaffold(
+    body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(border: Border.all(color: Colors.red, width: 2.0)),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+              automaticallyImplyLeading: true,
+              backgroundColor: Colors.white,
+              centerTitle: false,
+              forceElevated: true,
+              titleSpacing: 0.0,
+              pinned: false,
+              title: Text("添加朋友",
+                  style: TextStyle(color: Colors.black))
+          ),
+          SliverFillRemaining(
+            child: Center(child: Text('FillRemaining', style: TextStyle(fontSize: 30.0))),
+          )
+        ],
+      ),
+    ),
+  );
 }
